9c6ec904ca482e37c5663d25a26753de4bc0efb9
Alexander Ramirez <alexramirez8962@gmail.com>
2024-03-20 20:23:35 -0400

    Grocery List

--- app/src/main/java/com/example/phase12/GroceryList.kt
+++ app/src/main/java/com/example/phase12/GroceryList.kt
@@ -1,12 +1,15 @@
 package com.example.phase12
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.viewbinding.ViewBinding
 import com.example.phase12.databinding.GroceryListBinding
 
 
 class GroceryList : toolbar() {
     private lateinit var binding: ViewBinding
+    private lateinit var fab: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +17,13 @@ class GroceryList : toolbar() {
 
         binding = GroceryListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        fab = findViewById<View>(R.id.fab_grocery_list)
 
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        fab.setOnClickListener {
+            // Respond to FAB click
+            true
+        }
     }
 }
