a6bed718711994d2170455285e914d0371c8bc74
claireneace <claire.neace@gmail.com>
2024-03-26 23:24:13 -0400

    dear god help me

--- app/src/main/java/com/example/phase12/MealPrep.kt
+++ app/src/main/java/com/example/phase12/MealPrep.kt
@@ -1,11 +1,25 @@
 package com.example.phase12
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.viewbinding.ViewBinding
+import android.widget.CalendarView
+import android.widget.CalendarView.OnDateChangeListener
+import android.widget.TextView
 import com.example.phase12.databinding.MealPrepBinding
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.FileNotFoundException
+
 
 class MealPrep : toolbar() {
     private lateinit var binding: ViewBinding
+    lateinit var dateTV: TextView
+    private var recipeArray: MutableList<View> = mutableListOf()
+    lateinit var calendarView: CalendarView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +30,21 @@ class MealPrep : toolbar() {
 
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //Calender View Implementation from GeeksforGeeks
+
+        dateTV = findViewById(R.id.textView)
+        calendarView = findViewById(R.id.calendarView)
+
+
+        calendarView
+            .setOnDateChangeListener(
+
+                OnDateChangeListener { view, year, month, dayOfMonth ->
+                    val Date = (dayOfMonth.toString() + "-"
+                            + (month + 1) + "-" + year)
+                    dateTV.setText(Date)
+
+                })
     }
 }
