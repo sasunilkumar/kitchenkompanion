3e6ebc5413bc2dcbd4bf2a0b3c8c4e620fb6bbbd
Sneha Sunilkumar <snesun@terpmail.umd.edu>
2024-04-07 18:33:57 -0400

    changed font sizes

--- app/src/main/java/com/example/phase12/Home.kt
+++ app/src/main/java/com/example/phase12/Home.kt
@@ -3,19 +3,21 @@ package com.example.phase12
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.phase12.databinding.HomeBinding
 import androidx.viewbinding.ViewBinding
+import com.google.android.material.bottomappbar.BottomAppBar
 
 class Home : AppCompatActivity() {
     private lateinit var binding: ViewBinding
 
-    private lateinit var expenses: Button
     private lateinit var recipes: Button
     private lateinit var inventory: Button
     private lateinit var grocery: Button
     private lateinit var mealPrep: Button
+    private lateinit var bottomAppBar: BottomAppBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +38,7 @@ class Home : AppCompatActivity() {
         grocery.setOnClickListener { startActivity(Intent(this, GroceryList::class.java)) }
         mealPrep.setOnClickListener {startActivity(Intent(this, MealPrep::class.java)) }
 
-
+        val appBar = findViewById<BottomAppBar>(R.id.bottomAppBar)
     }
 
 }
--- app/src/main/java/com/example/phase12/ui/theme/AppBar.kt
+++ app/src/main/java/com/example/phase12/ui/theme/AppBar.kt
@@ -3,6 +3,7 @@ package com.example.phase12.ui.theme
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.example.phase12.GroceryList
 import com.example.phase12.Home
@@ -23,6 +24,7 @@ open class AppBar: AppCompatActivity() {
         appBar = findViewById(R.id.bottomAppBar)
         appBar.setNavigationOnClickListener {
         }
+
         appBar.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.bot_Recipes -> {
@@ -30,31 +32,31 @@ open class AppBar: AppCompatActivity() {
                     startActivity(Intent(this, Recipes::class.java))
                     true
                 }
+
                 R.id.bot_GroceryList -> {
                     Log.d("ACTION MENU", "bot_GroceryList")
                     startActivity(Intent(this, GroceryList::class.java))
                     true
                 }
+
                 R.id.bot_InventoryList -> {
                     Log.d("ACTION MENU", "bot_InventoryList")
                     startActivity(Intent(this, InventoryList::class.java))
                     true
                 }
-                R.id.bot_Profile -> {
-                    Log.d("ACTION MENU", "bot_Profile")
-                    startActivity(Intent(this, Profile::class.java))
-                    true
-                }
+
                 R.id.bot_MealPrep -> {
                     Log.d("ACTION MENU", "bot_MealPrep")
                     startActivity(Intent(this, MealPrep::class.java))
                     true
                 }
+
                 R.id.bot_Home -> {
                     Log.d("ACTION MENU", "bot_Home")
                     startActivity(Intent(this, Home::class.java))
                     true
                 }
+
                 else -> false
             }
         }
