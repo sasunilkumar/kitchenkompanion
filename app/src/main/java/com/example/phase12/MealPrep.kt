f15fa04366fe82822dfbdf7426aeedc0409fb906
claireneace <claire.neace@gmail.com>
2024-04-08 23:09:46 -0400

    what $1.50 buys...

--- app/src/main/java/com/example/phase12/MealPrep.kt
+++ app/src/main/java/com/example/phase12/MealPrep.kt
@@ -9,7 +9,10 @@ import android.widget.CalendarView.OnDateChangeListener;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AlertDialog;
+import androidx.cardview.widget.CardView
 import com.example.phase12.databinding.MealPrepBinding;
 import com.example.phase12.ui.theme.AppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -20,52 +23,75 @@ import java.io.FileNotFoundException;
 
 class MealPrep : AppBar() {
     private lateinit var binding: MealPrepBinding;
-    lateinit var dateTV: TextView;
+    private lateinit var monday_meals: LinearLayout
+    private lateinit var tuesday_meals: LinearLayout
+    private lateinit var wednesday_meals: LinearLayout
+    private lateinit var thursday_meals: LinearLayout
+    private lateinit var friday_meals: LinearLayout
+    private lateinit var saturday_meals: LinearLayout
+    private lateinit var sunday_meals: LinearLayout
+    private lateinit var monday: CardView
+    private lateinit var tuesday: CardView
+    private lateinit var wednesday: CardView
+    private lateinit var thursday: CardView
+    private lateinit var friday: CardView
+    private lateinit var saturday: CardView
+    private lateinit var sunday: CardView
+    private lateinit var arrowDown: ImageView
+    private lateinit var arrowUp: ImageView
     private var recipeArray: MutableList<View> = mutableListOf();
-//    lateinit var calendarView: CalendarView;
     lateinit var addButton: FloatingActionButton;
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        binding = MealPrepBinding.inflate(layoutInflater);
-        setContentView(binding.root);
-        addButton = findViewById(R.id.fab_meal_prep_list)
+        super.onCreate(savedInstanceState)
+        binding = MealPrepBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupBar()
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+        monday_meals = findViewById(R.id.monday_layouts_vertical)
+        tuesday_meals = findViewById(R.id.tuesday_layouts_vertical)
+        wednesday_meals = findViewById(R.id.wednesday_layouts_vertical)
+        thursday_meals = findViewById(R.id.thursday_layouts_vertical)
+        friday_meals = findViewById(R.id.friday_layouts_vertical)
+        saturday_meals = findViewById(R.id.saturday_layouts_vertical)
+        sunday_meals = findViewById(R.id.sunday_layouts_vertical)
 
-        dateTV = binding.textView;
-//        calendarView = binding.calendarView;
-//        addButton = binding.addMealButton;
+        monday = findViewById(R.id.monday_card)
+        tuesday = findViewById(R.id.tuesday_card)
+        wednesday = findViewById(R.id.wednesday_card)
+        thursday = findViewById(R.id.thursday_card)
+        friday = findViewById(R.id.friday_card)
+        saturday = findViewById(R.id.saturday_card)
+        sunday = findViewById(R.id.sunday_card)
 
-//        calendarView.setOnDateChangeListener(
-//            OnDateChangeListener { view, year, month, dayOfMonth ->
-//                val date = dayOfMonth.toString() + "-" + (month + 1) + "-" + year;
-//                dateTV.setText(date);
-//            }
-//        );
+
+
+        addButton = findViewById(R.id.add_button)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         addButton.setOnClickListener {
-            showAddMealDialog();
+            showAddMealDialog()
         }
     }
 
     private fun showAddMealDialog() {
-        val addMealLayout = LayoutInflater.from(this).inflate(R.layout.add_meal_dialog, null);
-        val mealNameEditText = addMealLayout.findViewById<EditText>(R.id.meal_name_edittext);
-        val caloriesEditText = addMealLayout.findViewById<EditText>(R.id.calories_edittext);
-        val sugarEditText = addMealLayout.findViewById<EditText>(R.id.sugar_edittext);
+        val addMealLayout = LayoutInflater.from(this).inflate(R.layout.add_meal_dialog, null)
+        val mealNameEditText = addMealLayout.findViewById<EditText>(R.id.meal_name_edittext)
+        val caloriesEditText = addMealLayout.findViewById<EditText>(R.id.calories_edittext)
+        val sugarEditText = addMealLayout.findViewById<EditText>(R.id.sugar_edittext)
 
         AlertDialog.Builder(this)
             .setView(addMealLayout)
             .setTitle("Add Meal Entry")
             .setPositiveButton("Add") { dialog, id ->
-                val mealName = mealNameEditText.text.toString();
-                val calories = caloriesEditText.text.toString();
-                val sugar = sugarEditText.text.toString();
-                Log.d("MealPrep", "Meal added: $mealName, Calories: $calories, Sugar: $sugar");
+                val mealName = mealNameEditText.text.toString()
+                val calories = caloriesEditText.text.toString()
+                val sugar = sugarEditText.text.toString()
+                Log.d("MealPrep", "Meal added: $mealName, Calories: $calories, Sugar: $sugar")
             }
             .setNegativeButton("Cancel", null)
             .create()
-            .show();
+            .show()
     }
 }
