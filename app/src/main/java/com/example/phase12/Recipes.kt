1668579aec6f60ffb08a1725241a5e33ffb75673
Sneha Sunilkumar <snesun@terpmail.umd.edu>
2024-04-11 19:46:33 -0400

    gave up on edittext fts

--- app/src/main/java/com/example/phase12/Recipes.kt
+++ app/src/main/java/com/example/phase12/Recipes.kt
@@ -39,6 +39,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.ui.node.getOrAddAdapter
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.marginLeft
 import androidx.core.view.marginTop
 import androidx.core.view.setPadding
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -663,16 +664,28 @@ class Recipes : AppBar() {
             checkParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE)
             checkItem.layoutParams = checkParams
             checkItem.gravity = Gravity.CENTER_VERTICAL
+            checkItem.setPadding(0,context.resources.getDimensionPixelSize(R.dimen.checkEditPadding), 0, 0)
+            checkItem.text = ingredient
+            checkItem.setTextColor(textColor)
+            checkItem.textSize = 20F
 
-            val checkEditText = EditText(context)
-
-            checkEditText.setText(ingredient)
-            checkEditText.setTextColor(textColor)
-            checkEditText.textSize = 20F
-            checkEditText.gravity = Gravity.CENTER_VERTICAL
+//            val checkEditText = EditText(context)
+//
+//            val checkEditParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+//                RelativeLayout.LayoutParams.WRAP_CONTENT)
+//
+//            checkEditParams.addRule(RelativeLayout.ALIGN_END, checkItem.id)
+//            checkEditParams.leftMargin = context.resources.getDimensionPixelSize(R.dimen.checkText)
+//            checkItem.layoutParams = checkParams
+//
+//            checkEditText.setText(ingredient)
+//            checkEditText.setTextColor(textColor)
+//            checkEditText.textSize = 20F
+//            checkEditText.gravity = Gravity.CENTER_VERTICAL
+            //checkEditText.setPadding(context.resources.getDimensionPixelSize(R.dimen.checkText),0,0, 0)
 
             newItem.addView(checkItem)
-            newItem.addView(checkEditText)
+//            newItem.addView(checkEditText)
 
             ingredientsRelLayout.addView(newItem)
             prevId = newItem.id
